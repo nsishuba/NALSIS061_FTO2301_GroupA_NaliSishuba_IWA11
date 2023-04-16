7e4624e7e24fe0a821e9c3258079619ceac2164b
nsishuba <nalisishuba@gmail.com>
2023-04-16 19:23:44 +0200

    Added getElementByClassName, getAttribute.

--- scripts.js
+++ scripts.js
@@ -1,34 +1,34 @@
+const root1 = document.querySelector('[data-key = "order1"]') //retrieves the first element with said property
+const biscuits1 = document.getElementsByClassName('biscuits') //checks all elements and retrieves all elements with said class name in array form
+const donuts1 = document.getElementsByClassName('donuts')
+const pancakes1 = document.getElementsByClassName('pancakes')
+const status1 = document.getElementsByClassName('status')
+const root2 = document.querySelector('[data-key = "order2"]')
+const root3 = document.querySelector('[data-key = "order3"]')
 
-const root1 = document.querySelector("[data-key = order1]");
-let biscuits1 = document.querySelector(".biscuit");
-let donuts1 = document.querySelector(".donuts");
-let pancakes1 = document.querySelector(".pancakes");
-let status1 = document.querySelector(".status");
+const bis1 = biscuits1[0].getElementsByTagName("dd")
+bis1[0].innerText = root1.getAttribute("data-biscuits")
+const don1 = donuts1[0].getElementsByTagName("dd")
+don1[0].innerText = root1.getAttribute("data-donuts")
+const pan1 = pancakes1[0].getElementsByTagName("dd")
+pan1[0].innerText = root1.getAttribute("data-pancakes")
+const stat1 = status1[0].getElementsByTagName("dd")
+stat1[0].innerText = (root1.getAttribute("data-delivered"))? "Delivered" : "Pending"
 
-const root2 = document.querySelector("[data-key = order2]");
-let biscuits2 = document.querySelector(".biscuits");
-let donuts2 = document.querySelector(".donuts");
-let pancakes2 = document.querySelector(".pancakes");
-let status = document.querySelector(".status");
+const bis2 = biscuits1[1].getElementsByTagName("dd")
+bis2[0].innerText = root2.getAttribute("data-biscuits")
+const don2 = donuts1[1].getElementsByTagName("dd")
+don2[0].innerText = root2.getAttribute("data-donuts")
+const pan2 = pancakes1[1].getElementsByTagName("dd")
+pan2[0].innerText = root2.getAttribute("data-pancakes")
+const stat2 = status1[1].getElementsByTagName("dd")
+stat2[0].innerText = (root2.getAttribute("data-delivered"))? "Delivered" : "Pending"
 
-const root3 = document.querySelector("[data-key = order3]");
-let biscuits3 = document.querySelector(".biscuits");
-let donuts3 = document.querySelector(".donuts");
-let pancakes3 = document.querySelector(".pancakes");
-let status3 = document.querySelector(".status");
-
-// let biscuitsCount = document.querySelector("dd.count")
-biscuits1 = root1.getAttribute("data-biscuits");
-donuts1 = root1.getAttribute("data-donuts");
-pancakes1 = root1.getAttribute("data-pancakes");
-status1 = root1.getAttribute("data-status") ? "Delivered" : "Pending"
-
-biscuits1 = root2.getAttribute("data-biscuits");
-donuts1 = root2.getAttribute("data-donuts");
-pancakes1 = root2.getAttribute("data-pancakes");
-status1 = root2.getAttribute("data-status") ? "Delivered" : "Pending"
-
-biscuits3 = root3.getAttribute("data-biscuits");
-donuts3 = root3.getAttribute("data-donuts");
-pancakes3 = root3.getAttribute("data-pancakes");
-status3 = root3.getAttribute("data-status") ? "Delivered" : "Pending"
+const bis3 = biscuits1[2].getElementsByTagName("dd")
+bis3[0].innerText = root3.getAttribute("data-biscuits")
+const don3 = donuts1[2].getElementsByTagName("dd")
+don3[0].innerText = root3.getAttribute("data-donuts")
+const pan3 = pancakes1[2].getElementsByTagName("dd")
+pan3[0].innerText = root3.getAttribute("data-pancakes")
+const stat3 = status1[2].getElementsByTagName("dd")
+stat3[0].innerText = (root3.getAttribute("data-delivered"))? "Delivered" : "Pending"
